29891f74c1c43975fe1512d4b40f07602810ed14
Austin A <austinandrews89@gmail.com>
2022-02-24 13:03:59 -0700

    fleshed out the barebones of the fetch function for pulling details of array position 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,6 +114,9 @@ function init() {
   handleAutocompleteDisplay();
 }
 
+
+var y = 0;
+
 function cardFetch(){
 
   var cardInfoEl = $("#pokemonCardInfoBox");
@@ -136,6 +139,17 @@ function cardFetch(){
     })
     .then(function (response){
       console.log(response);
+
+      console.log(response.data[y].images.small)
+      cardInfoEl.append("<img id='pokemonCardPic' src='"+response.data[y].images.small+"'></img>")
+      .append("<p>"+response.data[y].name+"</p>")
+      .append("<p>By artist: "+response.data[y].artist+"</p>")
+      .append("<p>Set: "+response.data[y].set.series+" - "+ response.data[y].set.name+"</p>")
+      .append("<p>Rarity: "+response.data[y].rarity)
+
+
+
+
     })
 
 }
